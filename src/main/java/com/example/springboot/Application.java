00cd4d8ce49eb03862cb57e132db73395c5f229a
Toshiaki Maki <tmaki@vmware.com>
2022-01-13 01:02:53 +0900

    init

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+    	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
+}
